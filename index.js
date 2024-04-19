441c9a6fe2e4884c90e63710eebed09beea2efcb
Muqaddas Fatima <160252102+MuqaddasFatima24@users.noreply.github.com>
2024-04-19 17:22:03 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+const systemGeneratedNo = Math.floor(Math.random() * 10);
+const answers = await inquirer.prompt([
+    {
+        type: "number",
+        name: "userGuess",
+        message: "write your guess between 1 to 10: ",
+    }
+]);
+const { userGuess } = answers;
+if (userGuess === systemGeneratedNo) {
+    console.log(userGuess, "userGuess", systemGeneratedNo, "SYs");
+    console.log("YEAAAA YOUR ANSWER IS CORRECT \ & YOU WINNN!");
+}
+else {
+    console.log("please try again BETTER LUCK next time");
+}
